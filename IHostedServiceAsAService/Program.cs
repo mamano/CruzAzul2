35f02245b382359e2309c6da29f2957760a99e95
marcel amano <mar_amano@hotmail.com>
2019-06-06 01:07:00 -0300

    new project

---
+++ IHostedServiceAsAService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Dicom.Network;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IHostedServiceAsAService
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var server = DicomServer.Create<CStoreSCP>(11112);
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<FileWriterService>();
+                });
+
+            if (isService)
+            {
+                await builder.RunAsServiceAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
+        }
+    }
+}
---
+++ IHostedServiceAsAService/ServiceBaseLifetime.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dicom;
+using Dicom.Log;
+using Dicom.Network;
+using Microsoft.Extensions.Hosting;
+
+namespace IHostedServiceAsAService
+{
+    // Code from https://github.com/aspnet/Hosting/blob/2a98db6a73512b8e36f55a1e6678461c34f4cc4d/samples/GenericHostSample/ServiceBaseLifetime.cs
+
+    public class ServiceBaseLifetime : ServiceBase, IHostLifetime
+    {
+        private readonly TaskCompletionSource<object> _delayStart = new TaskCompletionSource<object>();
+
+        public ServiceBaseLifetime(IApplicationLifetime applicationLifetime)
+        {
+            ApplicationLifetime = applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime));
+        }
+
+        private IApplicationLifetime ApplicationLifetime { get; }
+
+        public Task WaitForStartAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(() => _delayStart.TrySetCanceled());
+            ApplicationLifetime.ApplicationStopping.Register(Stop);
+
+            new Thread(Run).Start(); // Otherwise this would block and prevent IHost.StartAsync from finishing.
+            return _delayStart.Task;
+        }
+
+        private void Run()
+        {
+            try
+            {
+                Run(this); // This blocks until the service is stopped.
+                _delayStart.TrySetException(new InvalidOperationException("Stopped without starting"));
+            }
+            catch (Exception ex)
+            {
+                _delayStart.TrySetException(ex);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Stop();
+            return Task.CompletedTask;
+        }
+
+        // Called by base.Run when the service is ready to start.
+        protected override void OnStart(string[] args)
+        {
+            _delayStart.TrySetResult(null);
+            base.OnStart(args);
+        }
+
+        // Called by base.Stop. This may be called multiple times by service Stop, ApplicationStopping, and StopAsync.
+        // That's OK because StopApplication uses a CancellationTokenSource and prevents any recursion.
+        protected override void OnStop()
+        {
+            ApplicationLifetime.StopApplication();
+            base.OnStop();
+        }
+    }
+
+    public class CStoreSCP : DicomService, IDicomServiceProvider, IDicomCStoreProvider, IDicomCEchoProvider
+    {
+        private static DicomTransferSyntax[] AcceptedTransferSyntaxes = new DicomTransferSyntax[]
+                                                                                {
+                                                                                    DicomTransferSyntax
+                                                                                        .ExplicitVRLittleEndian,
+                                                                                    DicomTransferSyntax
+                                                                                        .ExplicitVRBigEndian,
+                                                                                    DicomTransferSyntax
+                                                                                        .ImplicitVRLittleEndian
+                                                                                };
+
+        private static DicomTransferSyntax[] AcceptedImageTransferSyntaxes = new DicomTransferSyntax[]
+                                                                                 {
+                                                                                         // Lossless
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEGLSLossless,
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEG2000Lossless,
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEGProcess14SV1,
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEGProcess14,
+                                                                                         DicomTransferSyntax
+                                                                                             .RLELossless,
+
+                                                                                         // Lossy
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEGLSNearLossless,
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEG2000Lossy,
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEGProcess1,
+                                                                                         DicomTransferSyntax
+                                                                                             .JPEGProcess2_4,
+
+                                                                                         // Uncompressed
+                                                                                         DicomTransferSyntax
+                                                                                             .ExplicitVRLittleEndian,
+                                                                                         DicomTransferSyntax
+                                                                                             .ExplicitVRBigEndian,
+                                                                                         DicomTransferSyntax
+                                                                                             .ImplicitVRLittleEndian
+                                                                                 };
+
+        public CStoreSCP(INetworkStream stream, Encoding fallbackEncoding, Logger log)
+            : base(stream, fallbackEncoding, log)
+        {
+        }
+
+        public Task OnReceiveAssociationRequestAsync(DicomAssociation association)
+        {
+            if (association.CalledAE != "STORESCP")
+            {
+                return SendAssociationRejectAsync(
+                    DicomRejectResult.Permanent,
+                    DicomRejectSource.ServiceUser,
+                    DicomRejectReason.CalledAENotRecognized);
+            }
+
+            foreach (var pc in association.PresentationContexts)
+            {
+                if (pc.AbstractSyntax == DicomUID.Verification) pc.AcceptTransferSyntaxes(AcceptedTransferSyntaxes);
+                else if (pc.AbstractSyntax.StorageCategory != DicomStorageCategory.None) pc.AcceptTransferSyntaxes(AcceptedImageTransferSyntaxes);
+            }
+
+            return SendAssociationAcceptAsync(association);
+        }
+
+        public Task OnReceiveAssociationReleaseRequestAsync()
+        {
+            return SendAssociationReleaseResponseAsync();
+        }
+
+        public void OnReceiveAbort(DicomAbortSource source, DicomAbortReason reason)
+        {
+        }
+
+        public void OnConnectionClosed(Exception exception)
+        {
+        }
+
+        public DicomCStoreResponse OnCStoreRequest(DicomCStoreRequest request)
+        {
+            var studyUid = request.Dataset.GetString(DicomTag.StudyInstanceUID);
+            var patientId = request.Dataset.GetString(DicomTag.PatientID);
+            var seriesNumber = request.Dataset.GetString(DicomTag.InstanceNumber);
+            var studyDate = request.Dataset.GetString(DicomTag.StudyDate);
+            var instUid = $"1.2.840.113619.2.81.290.{studyDate}.{patientId}";
+            request.Dataset.Remove(DicomTag.StudyInstanceUID);
+            request.Dataset.AddOrUpdate(DicomTag.StudyInstanceUID, instUid);
+            request.Dataset.Remove(DicomTag.SeriesInstanceUID);
+            request.Dataset.AddOrUpdate(DicomTag.SeriesInstanceUID, instUid + $".{seriesNumber}");
+            //var path = Path.GetFullPath(@".\DICOM");
+            var path = "C:\\INFINITT\\Spool";
+            path = Path.Combine(path, instUid + $".{seriesNumber}" + ".dcm");
+
+            //if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+            //path = Path.Combine(path, instUid) + ".dcm";
+
+            request.File.Save(path);
+
+            return new DicomCStoreResponse(request, DicomStatus.Success);
+        }
+
+        public void OnCStoreRequestException(string tempFileName, Exception e)
+        {
+            // let library handle logging and error response
+        }
+
+        public DicomCEchoResponse OnCEchoRequest(DicomCEchoRequest request)
+        {
+            return new DicomCEchoResponse(request, DicomStatus.Success);
+        }
+    }
+}
